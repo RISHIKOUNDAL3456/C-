bed66152555f003e660c8505a59d6700cc1ba027
RISHIKOUNDAL3456 <rishikoundal22@gmail.com>
2026-09-18 22:22:09 +0530

    Relational.c

---
+++ relational.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+    int x = 20, y = 25;
+
+    printf("The x < y: %d\n", x < y);
+    printf("The x > y: %d\n", x > y);
+    printf("The x <= y: %d\n", x <= y);
+    printf("The x >= y: %d\n", x >= y);
+    printf("The x == y: %d\n", x == y);
+    printf("The x != y: %d\n", x != y);
+
+    return 0;
+}
